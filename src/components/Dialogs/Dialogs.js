f6698d625be6c84bb3486c8c6a20c59a79b85cbd
Chingiz Shigayev <shigayevphone@gmail.com>
2020-10-28 21:44:27 +0400

    data mapped

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,36 +2,55 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import st from "./Dialogs.module.css";
 
+let dialogData = [
+  { id: 1, name: "jon" },
+  { id: 2, name: "sara" },
+  { id: 3, name: "nity" },
+  { id: 4, name: "kate" },
+  { id: 5, name: "suzy" },
+];
+
+let messagesData = [
+  { id: 1, message: "Hello" },
+  { id: 2, message: "What's up?" },
+  { id: 3, message: "React is awesome" },
+  { id: 4, message: "Need hepl" },
+  { id: 5, message: "Rojer that" },
+  { id: 6, message: "How dare you?" }
+];
+
 const Dialog = (props) => {
   return (
-    <div className={st.dialog + " " + st.dialogActive}>
+    <div className={st.dialog + " " + st.dialogActive} key={props.id}>
       <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
     </div>
   );
 };
 
 const Messages = (props) => {
-  return <div className={st.message}>{props.message}</div>;
+  return (
+    <div className={st.message} key={props.id}>
+      {props.message}
+    </div>
+  );
 };
+
+let messages = messagesData.map((m) => (
+  <Messages key={m.id} message={m.message} />
+));
+
+let dialogs = dialogData.map((d) => <Dialog id={d.id} name={d.name} />);
 
 const Dialogs = () => {
   return (
     <div className={st.dialogs}>
       <div className={st.dialogsItems}>
         <h2>Dialogs</h2>
-        <Dialog id="1" name="jon" />
-        <Dialog id="2" name="sara" />
-        <Dialog id="3" name="nity" />
-        <Dialog id="4" name="kate" />
-        <Dialog id="5" name="suzy" />
+        {dialogs}
       </div>
       <div className={st.messages}>
         <h2>Messages</h2>
-        <Messages message="hellou" />
-        <Messages message="what's up" />
-        <Messages message="need help" />
-        <Messages message="rodjer that" />
-        <Messages message="how dare you?" />
+        { messages }
       </div>
     </div>
   );
--- src/components/Main/MyPosts/MyPosts.js
+++ src/components/Main/MyPosts/MyPosts.js
@@ -2,6 +2,16 @@ import React from "react";
 import st from "./MyPosts.module.css";
 import Post from "./Post/Post.js";
 
+let postData = [
+  { id: 1, text: "learn React", likes: 7 },
+  { id: 2, text: "awesome", likes: 3 },
+  { id: 1, text: "learn Redux", likes: 11 },
+];
+
+let posts = postData.map((p) => (
+  <Post key={p.id} text={p.text} likes={p.likes} />
+));
+
 const MyPosts = () => {
   return (
     <div>
@@ -11,10 +21,7 @@ const MyPosts = () => {
       <div>
         <button>add post</button>
       </div>
-      <div>
-        <Post text="learn REACT" likes="7" />
-        <Post text="awesome" likes="9" />
-      </div>
+      <div>{posts}</div>
     </div>
   );
 };
